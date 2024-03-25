830d9de349e3cc480641b0683159e22dec379cf7
Eknath kalkekar <kalkekare@gmail.com>
2024-03-25 20:05:58 +0530

    one line delete

--- DemoApplication.java
+++ DemoApplication.java
@@ -9,7 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		String a=null;
-                int a =10+20;
                 int b=10*30;
                 int c=50-12;
 	
